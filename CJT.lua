2a1658ae195fd54949ce9247a03f946a54a22881
huliganhak <133084905+huliganhak@users.noreply.github.com>
2025-06-19 16:29:37 +0700

    Update CJT.lua

--- CJT.lua
+++ CJT.lua
@@ -98,6 +98,7 @@ local Rejoinsection1 = page:addSection("Rejoin Server Setting")
 local textRejoin = Rejoinsection1:addWideLabel("สถานะ...", Color3.fromRGB(255, 0, 0))
 Rejoinsection1:addButton("Search Server", function()
 	print("Rejoin Button Click")
+	print("ข้อความปัจจุบันคือ:", textRejoin.Label.Text)
 end)
 Rejoinsection1:addButton("Rejoin Server", function(value)
         
